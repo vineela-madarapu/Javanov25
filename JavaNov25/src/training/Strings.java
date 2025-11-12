646386bf55b0a9bc6fc84671869c82e03e669c99
vinny <vinny@localhost>
2025-11-12 17:40:15 -0600

    new

---
+++ JavaNov25/src/training/Strings.java
@@ -0,0 +1,16 @@
+package training;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String str = "Selenium";
+		String str1 = new String("Selenium");
+		System.out.println(str==str1);
+		
+		//String,Stringbuffer,Stringbuilder
+	}
+
+}
